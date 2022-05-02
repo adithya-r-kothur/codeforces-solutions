a45687ff650b1a03a747ae1952ea5cbdad156e9c
adithya <adithya.r.kothur@gmail.com>
2022-05-02 12:10:22 +0530

    initial commit

---
+++ wordcapitalization.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+
+    if(s[0]>96)
+    {
+        s[0]-=32;
+    }
+
+    cout<<s<<"\n";
+    return 0;
+}
