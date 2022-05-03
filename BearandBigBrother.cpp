05176eba8e5fbb3ed16e2807dda3d3a3543f6295
adithya <adithya.r.kothur@gmail.com>
2022-05-03 14:35:45 +0530

    initial commit

---
+++ BearandBigBrother.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+
+    int i=0;
+    while(a<=b)
+    {
+       a*=3;
+       b*=2;
+       i++; 
+    }
+
+    cout<<i<<"\n";
+    return 0;
+}
