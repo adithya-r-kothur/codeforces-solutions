8c978df707e5403dc4fdd1c771a6ea05c8c10fcf
adithya <adithya.r.kothur@gmail.com>
2022-05-05 15:59:03 +0530

    initial commit

---
+++ 467A-GeorgeandAccommodation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int ans=0;
+
+    for(int i=0;i<n;i++)
+    {
+        int p,q;
+        cin>>p>>q;
+        if(q-p>=2)
+        {
+            ans++;
+        }
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
