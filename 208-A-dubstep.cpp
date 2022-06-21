9484c0585a8e2cf68b38940f474bd5474391bfce
adithya <adithya.r.kothur@gmail.com>
2022-06-21 23:43:32 +0530

    initial commit

--- 208-A-dubstep.cpp
+++ 208-A-dubstep.cpp
@@ -1,9 +1,21 @@
-#include<bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    
-    return 0;
+int main() {
+	string s;
+	int flag=1;
+	cin>>s;
+	for(int i=0;i<s.size();i++){
+	    if(s[i]=='W'&& s[i+1]=='U' && s[i+2]=='B'){
+	        i+=2;
+	        if(!flag){
+	            cout<<" ";
+	        }
+	        continue;
+	    }else {
+	        flag=0;
+	        cout<<s[i];
+	    }
+	}
+	return 0;
 }
