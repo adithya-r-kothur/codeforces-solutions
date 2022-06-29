5af1dc051245821bc7183ae387aceaa8910cd9aa
adithya <adithya.r.kothur@gmail.com>
2022-06-29 13:53:36 +0530

    initial commit

--- 230-A-Dragons.cpp
+++ 230-A-Dragons.cpp
@@ -4,7 +4,33 @@ using namespace std;
 
 int main()
 {
-    
+    int n,s;
+
+    cin>>s>>n;
+
+    int x[n];
+
+    int y[n];
+
+    for(int i=0;i<n;i++)
+    {
+       cin>>x[i]>>y[i];
+    }
+
+    sort(x,x+n,greater<int>());
+    //sort(y,y+n,greater<int>());
+
+    if(s<x[n-1])
+    {
+        cout<<"NO\n";
+    }
+    else
+    {
+
+    }
+
+
+
+
     return 0;
 }
-
